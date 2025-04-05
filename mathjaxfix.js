d6d74c21a99c5c10b06d02b744c2f6c00c5f119f
kzschool <kilgorezer2nd@hotmail.com>
2025-04-04 22:42:57 -0700

    Update mathjaxfix.js
    Added updater

--- mathjaxfix.js
+++ mathjaxfix.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         MathJax Fix
-// @namespace    http://tampermonkey.net/
-// @version      1.0
+// @namespace    http://tpviewer.kilgorezer.com/
+// @version      1.1
 // @description  Fix MathJax on Connexus on Supermium
 // @author       kilgorezer
 // @match        https://prodpcx-cdn-vegaviewer.emssvc.connexus.com/index.html
 // @icon         https://www.connexus.com/favicon.ico
+// @downloadURL  https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.js
+// @updateURL    https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.js
 // @grant        none
 // ==/UserScript==
 
