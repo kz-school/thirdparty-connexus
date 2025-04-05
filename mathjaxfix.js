9a767ac52f05bbe1e948caef7b8060fd3cd64c54
kzschool <kilgorezer2nd@hotmail.com>
2025-04-04 22:52:01 -0700

    Update mathjaxfix.js
    Finally fix the match problem

--- mathjaxfix.js
+++ mathjaxfix.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         MathJax Fix
 // @namespace    http://tpviewer.kilgorezer.com/
-// @version      1.2
+// @version      1.3
 // @description  Fix MathJax on Connexus on Supermium
 // @author       kilgorezer
-// @match        https://prodpcx-cdn-vegaviewer.emssvc.connexus.com/index.html
+// @match        https://prodpcx-cdn-vegaviewer.emssvc.connexus.com/index.html?*
 // @icon         https://www.connexus.com/favicon.ico
 // @downloadURL  https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.js
 // @updateURL    https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.js
@@ -12,5 +12,5 @@
 // ==/UserScript==
 
 (function() {
-    setInterval(a=>{window.MathJax.Hub.queue.pending=1;}, 1000);
+    setTimeout(a=>{window.MathJax.Hub.queue.pending=1;}, 1000);
 })();
