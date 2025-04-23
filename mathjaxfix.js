ebf1332b4114a7597e43a11912daab73bb5514f0
kzschool <kilgorezer2nd@hotmail.com>
2025-04-23 12:52:20 -0700

    Update and rename mathjaxfix.user.js to mathjaxfix.js

--- mathjaxfix.js
+++ mathjaxfix.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         MathJax Fix
 // @namespace    http://tpviewer.kilgorezer.com/
-// @version      1.3
+// @version      1.4
 // @description  Fix MathJax on Connexus on Supermium
 // @author       kilgorezer
 // @match        https://prodpcx-cdn-vegaviewer.emssvc.connexus.com/index.html?*
 // @icon         https://www.connexus.com/favicon.ico
-// @downloadURL  https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.js
-// @updateURL    https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.js
+// @downloadURL  https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.user.js
+// @updateURL    https://github.com/kz-school/thirdparty-connexus/raw/refs/heads/main/mathjaxfix.user.js
 // @grant        none
 // ==/UserScript==
 
